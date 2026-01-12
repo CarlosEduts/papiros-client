f60c87887fd855a414012d587ce158e0710ff87c
Carlos Silva <cedut.dev@gmail.com>
2026-01-12 18:39:32 -0300

    feat(client): implementa interfaces de login e registro com shadcn/ui

---
+++ components/login-form.tsx
@@ -0,0 +1,66 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Bem-vindo de Volta!</CardTitle>
+          <CardDescription>
+            Insira suas credenciais para acessar sua conta
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="email@exemplo.com"
+                  required
+                />
+              </Field>
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Senha</FieldLabel>
+                </div>
+                <Input id="password" type="password" required />
+              </Field>
+              <Field>
+                <Button type="submit">Entrar</Button>
+                <FieldDescription className="text-center">
+                  Não tem uma conta? <Link href="/register">Cadastre-se</Link>.
+                </FieldDescription>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+      <FieldDescription className="px-6 text-center">
+        Documentação do projeto no{" "}
+        <a href="https://github.com/CarlosEduts/papiros-server">GitHub</a>.
+      </FieldDescription>
+    </div>
+  );
+}
